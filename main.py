5ac06a8ea9dbdb2af6243936346622f92df266d2
RGS_DEV <ianjmarshall9@gmail.com>
2024-09-21 18:47:47 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from time import *
+def update():
+    timestr = strftime("%b %d   |   %I:%M %p")
+    TimeL.config(text=timestr,)
+    TimeL.after(1,update)
+window = Tk()
+icon = PhotoImage(file='clock.png')
+window.iconphoto(True,icon)
+window.resizable(False,False)
+window.title("Random's clock app")
+TimeL = Label(window,font=("Arial",50),fg="#00ff2a",bg="black",)
+TimeL.pack()
+update()
+window.mainloop()
